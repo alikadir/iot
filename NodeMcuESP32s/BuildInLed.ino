6254a3b056e5cc6fa8babe9c4fb9e52407ec1020
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2023-10-21 19:14:29 +0300

    Update BuildInLed.ino

--- NodeMcuESP32s/BuildInLed.ino
+++ NodeMcuESP32s/BuildInLed.ino
@@ -1,11 +1,15 @@
-/*
-
-*/
+#define INTERNAL_LED 2
 
 void setup() {
-    
+  pinMode(INTERNAL_LED,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    
+  digitalWrite(INTERNAL_LED,HIGH);
+  Serial.println("LED ON");
+  delay(1000);
+  digitalWrite(INTERNAL_LED,LOW);
+  Serial.println("LED OFF");
+  delay(1000);
 }
