ab23cadb8d622036a128582ab00aae092c869ebf
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2024-04-06 14:42:48 +0300

    Update RELAY.h

--- ESP32/desk-automation-applehomekit/RELAY.h
+++ ESP32/desk-automation-applehomekit/RELAY.h
@@ -1,27 +1,65 @@
+#include "Utils.h"
 #include <millisDelay.h>
 
 struct RELAY : Service::Switch {  // deriving from the HomeSpan Switch service
 
   int relayPin;
   int timeOutMilliseconds;
+  int buttonPin;
+
+  int buttonLastState = LOW;
+  int buttonCurrentState;
+
   SpanCharacteristic *power;
   millisDelay switchDelay;
+  millisDelay buttonDelay;
 
-  RELAY(int relayPin, int timeOutMilliseconds = 0) : Service::Switch() {
+  RELAY(int relayPin, int buttonPin = 0, int timeOutMilliseconds = 0)
+    : Service::Switch() {
 
     power = new Characteristic::On();
     this->relayPin = relayPin;
     this->timeOutMilliseconds = timeOutMilliseconds;
+    this->buttonPin = buttonPin;
     pinMode(relayPin, OUTPUT);
+    buttonDelay.start(10);
+
+    if (buttonPin != 0) {
+      pinMode(buttonPin, INPUT_PULLUP);
+    }
   }
 
   void loop() {
+
     if (switchDelay.justFinished()) {
-      
       power->setVal(false);
       digitalWrite(relayPin, LOW);
-      
       Serial.println("Single Delay finished.");
+    }
+
+    /////////////////////////////////////////////
+
+    if (buttonPin != 0) {
+      buttonCurrentState = digitalRead(buttonPin);
+      delay(100);
+      if (buttonLastState != buttonCurrentState) {
+        Serial.println("The button clicked");
+        if (buttonDelay.justFinished()) {
+          int val = power->getNewVal();
+          if (val == HIGH) {
+            power->setVal(false);
+            digitalWrite(relayPin, LOW);
+            Serial.println("Button Clicked. Close.");
+            buttonDelay.start(1000);
+          } else {
+            power->setVal(true);
+            digitalWrite(relayPin, HIGH);
+            Serial.println("Button Clicked. Open.");
+            buttonDelay.start(1000);
+          }
+        }
+      }
+      buttonLastState = buttonCurrentState;
     }
   }
 
@@ -36,7 +74,7 @@ struct RELAY : Service::Switch {  // deriving from the HomeSpan Switch service
     return (true);
   }
 
-  void print(){
+  void print() {
     //Serial.println("abc");
   }
 };
