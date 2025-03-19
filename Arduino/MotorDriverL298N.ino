c0c85e10605d859dac6bc2a449e1201e4d23192d
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2025-03-19 14:42:16 +0300

    Create MotorDriverL298N.ino

---
+++ Arduino/MotorDriverL298N.ino
@@ -0,0 +1,72 @@
+#define EN1 10
+
+#define IN1 9
+#define IN2 8
+#define IN3 7
+#define IN4 6
+
+#define EN2 5
+
+
+void setup() {
+  pinMode(EN1, OUTPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(EN2, OUTPUT);
+}
+
+void loop() {
+  goFast();
+  delay(5000);
+  goSlow();
+  delay(5000);
+
+  backFast();
+  delay(5000);
+  backSlow();
+  delay(5000);
+}
+
+void goSlow() {
+  analogWrite(EN1, 20);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  analogWrite(EN2, 20);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void goFast() {
+  analogWrite(EN1, 255);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  analogWrite(EN2, 255);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void backSlow() {
+  analogWrite(EN1, 20);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  analogWrite(EN2, 20);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backFast() {
+  analogWrite(EN1, 255);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  analogWrite(EN2, 255);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
