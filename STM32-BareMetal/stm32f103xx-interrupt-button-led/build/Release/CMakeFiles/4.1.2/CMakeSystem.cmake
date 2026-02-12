f2ec77f66cf48fdf89b56eb64eb64b8661b3e33f
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2026-02-13 00:21:30 +0300

    Add files via upload

---
+++ STM32-BareMetal/stm32f103xx-interrupt-button-led/build/Release/CMakeFiles/4.1.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.2.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.2.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/alikadir/Desktop/PROJECTS/stm32f103xx-interrupt-button-led/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
